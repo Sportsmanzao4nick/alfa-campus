a42b82365fc753e4216bd3c8153061c5ed57adc9
Sergey Luchko <serojaluchko@gmail.com>
2023-03-02 18:20:12 +0500

    add createAsyncThunk&fix redux problems

--- src/components/product-card/index.tsx
+++ src/components/product-card/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { ProductState } from "./types";
 import { useParams } from "react-router-dom";
 import { Product } from "../product-card-item";
--- src/components/products-list/made-in-alfa/index.tsx
+++ src/components/products-list/made-in-alfa/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Cell } from "../../products-list-item";
 import { Product } from "./types";
 import { NavLink } from "react-router-dom";
